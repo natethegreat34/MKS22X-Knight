2412e15afe6fe49ec1dafbf2ab83451ee52566f3
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-08 10:12:42 -0500

    Big oops

--- Place.java
+++ Place.java
@@ -1,12 +1,32 @@
-public class Place{
+public class Place implements Comparable<Place>{
 private int row;
 private int col;
 private int possible;
 
-public Place (int a, int b, int p){
+public Place (int a, int b){
     row = a;
     col = b;
-    possible = p;
 }
+
+public int getp (){
+ return possible;
+}
+
+public int getrow (){
+ return row;
+}
+public int getcol (){
+ return col;
+}
+public void setp (int n){
+ possible = n;
+}
+
+public int compareTo(Place o){
+    return possible - o.possible;
+}
+
+
+
 
 }
